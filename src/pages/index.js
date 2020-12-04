c639ae6d30f40519bd8ab450eebc4924db45c16c
talves <tony@3alves.com>
2020-12-04 11:19:07 -0800

    add nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Box } from "../components/box.js";
 import { Button } from "../components/index.js";
 
 export default (props) => (
-  <Box class="bg-fire-300">
+  <Box class="bg-fire-200">
     <Box class="flex flex-row">
       <Box class="flex-col w-1/2">
         <Box class="text-fire">Left-1</Box>
