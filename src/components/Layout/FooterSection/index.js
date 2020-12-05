291e4c44b62212cc082a628bc01c2edfdfcfb8ba
talves <tony@3alves.com>
2020-12-05 15:34:27 -0800

    data

--- src/components/Layout/FooterSection/index.js
+++ src/components/Layout/FooterSection/index.js
@@ -1,6 +1,7 @@
 import { h } from "preact";
 import { Box } from "../../box.js";
 import { Button } from "../../buttons/Button.js";
+import { useSiteData } from "../../SiteDataProvider/index.js";
 
 import TwitterIcon from "../../icons/social/twitter-icon.js";
 import GitHubIcon from "../../icons/social/github-icon.js";
@@ -26,18 +27,27 @@ const SocialLink = ({
 };
 
 const FooterSection = (props) => {
+  const data = useSiteData();
+  const { footer = {}, "site-metadata": siteMetadata = {} } = data;
+
   return (
     <Box>
       <Box class="flex flex-row">
         <Box as="span" class="w-1/4"></Box>
-        <SocialLink class="my-auto p-4" to={`https://twitter.com/3_alves`}>
+        <SocialLink
+          class="my-auto p-4"
+          to={`https://twitter.com/${siteMetadata.twitterUsername || ""}`}
+        >
           <TwitterIcon class="fill-current text-secondary-900 inline-block h-10 w-10" />
         </SocialLink>
-        <SocialLink class="my-auto p-4" to={`https://github.com/talves`}>
+        <SocialLink
+          class="my-auto p-4"
+          to={`https://github.com/${siteMetadata.githubUsername || ""}`}
+        >
           <GitHubIcon class="fill-current text-secondary-900 inline-block h-10 w-10" />
         </SocialLink>
         <Box class="my-auto p-4 text-secondary-900 font-bold inline-block">
-          © Tony Alves
+          © {footer.copyright}
         </Box>
       </Box>
     </Box>
