40f3c0e76ab7b234fc7c9aa7461f8d229435b389
talves <tony@3alves.com>
2020-12-06 13:10:40 -0800

    style

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import { Box } from "../components/box.js";
 import { Button } from "../components/index.js";
 
 export default (props) => (
-  <Box class=" min-h-full">
-    <Box class="flex lg:flex-row md:flex-col py-16">
-      <Box class="flex-col lg:w-1/2 md:w-full">
-        <Box class="lg:w-4/5 md:w-full m-auto bg-fire-200 bg-opacity-50 p-10 shadow-md border-r-4">
+  <Box class="min-h-full">
+    <Box class="flex items-center lg:flex-row md:flex-col py-16">
+      <Box class="flex-col lg:w-1/2 md:w-full lg:p-16 md:p-1 ">
+        <Box class="md:w-full m-auto bg-fire-200 bg-opacity-50 p-10 shadow-md border-r-4">
           <Box class="lg:text-5xl md:text-4xl text-fire-900 lg:leading-tight md:leading-none">
             <Box class="font-bold">
               Tools to help you keep your content available
@@ -22,8 +22,12 @@ export default (props) => (
           </Box>
         </Box>
       </Box>
-      <Box class="w-1/2">
-        <Box class="text-2xl text-gray-900">Right</Box>
+      <Box class="flex-col lg:w-1/2 md:w-full lg:p-32 md:p-20 h-full box-background">
+        <Box class="w-full text-center text-2xl text-gray-900">
+          Using the open source examples in the project, you can move your
+          content from your repository into a location where it can be accessed
+          by multiple sources.
+        </Box>
       </Box>
     </Box>
   </Box>
